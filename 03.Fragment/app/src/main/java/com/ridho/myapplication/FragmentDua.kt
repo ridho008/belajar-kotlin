fb74564ae7e408fddf47deb948fb46934a151665
ridho008 <ciuyy12@gmail.com>
2022-03-30 11:18:51 +0700

    mengirim data dari fragment 1 ke 2

--- 03.Fragment/app/src/main/java/com/ridho/myapplication/FragmentDua.kt
+++ 03.Fragment/app/src/main/java/com/ridho/myapplication/FragmentDua.kt
@@ -6,29 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentDua.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class FragmentDua : Fragment() {
-    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
+    //Latihan Mengirim Data Antar Fragment
+    lateinit var tvName: TextView
+    lateinit var tvDesc: TextView
+    lateinit var btnDua: Button
+
+    var desc: String? = null
+
+    companion object {
+        var EXTRA_NAME = "extra_name"
+        var EXTRA_DESC = "extra_desc"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,11 +31,13 @@ class FragmentDua : Fragment() {
         return inflater.inflate(R.layout.fragment_dua, container, false)
     }
 
-    override fun onViewCreated(view: View,
-                               savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btnDua: Button =
-            view.findViewById(R.id.btn_satu)
+
+        tvName= view.findViewById(R.id.tv_name)
+        tvDesc= view.findViewById(R.id.tv_desc)
+        btnDua= view.findViewById(R.id.btn_satu)
+
         val fragmentSatu = FragmentSatu()
         btnDua.setOnClickListener {
             fragmentManager?.beginTransaction()?.apply {
@@ -53,23 +48,26 @@ class FragmentDua : Fragment() {
         }
     }
 
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment FragmentDua.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            FragmentDua().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
+    override fun onActivityCreated(savedInstanceState: Bundle?): Unit {
+        super.onActivityCreated(savedInstanceState)
+        if (savedInstanceState != null) {
+            val description = savedInstanceState.getString(EXTRA_DESC)
+            desc = description
+        }
+
+        if(arguments != null)
+        {
+            val name = arguments?.getString(EXTRA_NAME)
+            tvName.text = name
+            tvDesc.text = desc
+        }
+    }
+
+
+
+
+        // END Latihan Mengirim Data Antar Fragment
+
+
+
 }
--- 03.Fragment/app/src/main/java/com/ridho/myapplication/FragmentSatu.kt
+++ 03.Fragment/app/src/main/java/com/ridho/myapplication/FragmentSatu.kt
@@ -9,14 +9,14 @@ import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentSatu.newInstance] factory method to
- * create an instance of this fragment.
- */
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
+//
+///**
+// * A simple [Fragment] subclass.
+// * Use the [FragmentSatu.newInstance] factory method to
+// * create an instance of this fragment.
+// */
 class FragmentSatu : Fragment() {
     // TODO: Rename and change types of parameters
 //    private var param1: String? = null
@@ -42,36 +42,22 @@ class FragmentSatu : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val btnDua: Button =
             view.findViewById(R.id.btn_dua)
+
         val fragmentDua = FragmentDua()
         btnDua.setOnClickListener {
+            val bundle = Bundle()
+            bundle.putString(FragmentDua.EXTRA_NAME,"Ini Data dari Fragment Satu")
+            val desc = "Fragment ini akan menerima data dari Fragment Satu"
+            fragmentDua.arguments=bundle
+            fragmentDua.desc=desc
+
             fragmentManager?.beginTransaction()?.apply {
-                replace(
-                    R.id.frame_container,
-                    fragmentDua, FragmentDua::class.java.simpleName
-                )
+                replace(R.id.frame_container, fragmentDua, FragmentDua::class.java.simpleName)
                     .addToBackStack(null)
                     .commit()
             }
         }
-    }
 
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment FragmentSatu.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            FragmentSatu().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
+
+    }
 }
