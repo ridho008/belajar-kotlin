7782aa852d0c8ce1c2ac59d66762aeb769d20b61
ridho008 <ciuyy12@gmail.com>
2022-06-28 16:39:44 +0700

    07-menambahkan bottom nav & trouble moviefragment.kt

--- 07-MovieCatalogue/app/src/main/java/com/ridho/moviecatalogue/MainActivity.kt
+++ 07-MovieCatalogue/app/src/main/java/com/ridho/moviecatalogue/MainActivity.kt
@@ -3,7 +3,12 @@ package com.ridho.moviecatalogue
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -16,6 +21,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomView)
+        val navController = findNavController(R.id.nav_host)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.movieFragment, R.id.tvShowFragmant, R.id.favoriteFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
 
         rv_movies_list.layoutManager = LinearLayoutManager(this)
         rv_movies_list.setHasFixedSize(true)
--- 07-MovieCatalogue/app/src/main/java/com/ridho/moviecatalogue/MovieFragment.kt
+++ 07-MovieCatalogue/app/src/main/java/com/ridho/moviecatalogue/MovieFragment.kt
@@ -7,39 +7,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_movie.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MovieFragment : Fragment() {
-    var movies: List<Movie> = null
+    var movies: List<Movie>? = null
     private lateinit var movieAdapter: MovieAdapter
+
     companion object {
         fun newInstance() = MovieFragment()
     }
-    private lateinit var viewModel: MovieViewModel
-    override fun onCreateView(
-        inflater: LayoutInflater, container:
-        ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return
-        inflater.inflate(R.layout.fragment_movie,
-            container, false)
-    }
-    override fun
-            onActivityCreated(savedInstanceState: Bundle?) {
+
+    private lateinit var viewModel: MovieFragment
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? { return inflater.inflate(R.layout.fragment_movie, container, false) }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MovieFragment::class.java)
-        rv_movies_list.layoutManager =
-            LinearLayoutManager(activity)
+        rv_movies_list.layoutManager = LinearLayoutManager(activity)
         rv_movies_list.setHasFixedSize(true)
-        getMovieData { movies: List&lt;Movie&gt; -&gt;
-            rv_movies_list.adapter =
-                MovieAdapter(movies, object :MovieAdapter.OnAdapterListener {
-                    override fun onClick(result: Movie)
-                    {
-                        val intent = Intent(activity,
-                            DetailMovieActivity::class.java)
-                        intent.putExtra(DetailMovieActivity.EXTRA_DATA,
-                            result)
+        getMovieData { movies: List<Movie> ->
+            rv_movies_list.adapter = MovieAdapter(movies, object :MovieAdapter.OnAdapterListener {
+
+                override fun onClick(result: Movie) {
+                        val intent = Intent(activity, DetailMovieActivity::class.java)
+                        intent.putExtra(DetailMovieActivity.EXTRA_DATA, result)
                         startActivity(intent)
                     }
                 })
@@ -62,17 +58,13 @@ class MovieFragment : Fragment() {
             adapter = movieAdapter
         }
     }
-    private fun getMovieData(callback: (List&lt;Movie&gt;) -&gt; Unit){
-        val apiService =
-            MovieApiService.getInstance().create(MovieApiInterf
-                    ace::class.java)
-        apiService.getMovieList().enqueue(object :
-            Callback&lt;MovieResponse&gt; {
-            override fun onFailure(call:
-                                   Call&lt;MovieResponse&gt;, t: Throwable) {
+
+    private fun getMovieData(callback: (List<Movie>) -> Unit){
+        val apiService = MovieApiService.getInstance().create(MovieApiInterface::class.java)
+        apiService.getMovieList().enqueue(object : Callback<MovieResponse> {
+            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
         }
-            override fun onResponse(call:Call&lt;MovieResponse&gt;, response:
-            Response&lt;MovieResponse&gt;) {
+            override fun onResponse(call:Call<MovieResponse>, response: Response<MovieResponse>) {
             movies = response.body()!!.movies
             return
             callback(response.body()!!.movies)
@@ -80,3 +72,5 @@ class MovieFragment : Fragment() {
         })
     }
 }
+
+
